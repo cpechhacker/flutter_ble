e92113498f613602bce809bb5314ca5c25ac9b9b
cpechhacker <christian.pechhacker@gmail.com>
2021-02-28 10:21:46 +0100

    add Widget "Selection"

---
+++ lib/WidgetSelection.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ble_cp/stopwatch.dart';
+import 'package:flutter_ble_cp/WidgetGauge.dart';
+import 'package:flutter_ble_cp/WidgetColor.dart';
+
+
+class SelectionWidget extends StatefulWidget {
+  @override
+  _SelectionWidgetState createState() => _SelectionWidgetState();
+}
+
+
+class _SelectionWidgetState extends State {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Stopwatch'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                child: Text("Stopwatch"),
+                onPressed: _startStopwatch,
+                color: Colors.white,
+                textColor: Colors.blue,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                splashColor: Colors.grey,
+              ),
+              RaisedButton(
+                child: Text("Gauge"),
+                onPressed: _startGauge,
+                color: Colors.white,
+                textColor: Colors.blue,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                splashColor: Colors.grey,
+              ),
+              RaisedButton(
+                child: Text("Colour"),
+                onPressed: _startColour,
+                color: Colors.white,
+                textColor: Colors.blue,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                splashColor: Colors.grey,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // This code is called when the first button is pressed.
+  // This code is called when the second button is pressed.
+  void _startStopwatch() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) =>StopwatchWidget()),
+    );
+  }
+
+  void _startGauge() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) =>GaugeApp()),
+    );
+  }
+
+  void _startColour() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) =>ColorPickerCP()),
+    );
+  }
+
+}
