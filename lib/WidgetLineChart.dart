be495bf5b87ff2bef264781978cf44a39632cc59
cpechhacker <christian.pechhacker@gmail.com>
2021-03-09 19:10:58 +0100

    test file

---
+++ lib/WidgetLineChart.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_ble_cp/fake_chart_series.dart';
+import 'package:fl_animated_linechart/fl_animated_linechart.dart';
+import 'package:flutter/material.dart';
+
+
+class LineChartApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Line Chart Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'fl_animated_chart demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with FakeChartSeries {
+  int chartIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Map<DateTime, double> line1 = createLine2();
+    Map<DateTime, double> line2 = createLine2_2();
+
+    LineChart chart;
+
+    if (chartIndex == 0) {
+      chart = LineChart.fromDateTimeMaps(
+          [line1, line2], [Colors.green, Colors.blue], ['C', 'C'],
+          tapTextFontWeight: FontWeight.w400);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    FlatButton(
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.black45),
+                          borderRadius: BorderRadius.all(Radius.circular(3))),
+                      child: Text(
+                        'LineChart',
+                        style: TextStyle(
+                            color: chartIndex == 0
+                                ? Colors.black
+                                : Colors.black12),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          chartIndex = 0;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AnimatedLineChart(
+                      chart,
+                      key: UniqueKey(),
+                    ), //Unique key to force animations
+                  )),
+              SizedBox(width: 200, height: 50, child: Text('')),
+            ]),
+      ),
+    );
+  }
+}
