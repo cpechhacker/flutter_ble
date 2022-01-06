9e0ac565c372a333c5f006a0f6b671203c0c394d
cpechhacker <christian.pechhacker@gmail.com>
2022-01-06 12:07:47 +0100

    widget testing

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:flutter_ble_cp/main.dart' as app;
+import 'file:///C:/Users/chris/AndroidStudioProjects/flutter_ble_cp/dart_alt/main.dart' as app;
 
 void main() => run(_testMain);
 
