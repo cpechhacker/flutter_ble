1a520bd920d02ab40fcb559e4361e726d282c88e
cpechhacker <christian.pechhacker@gmail.com>
2022-01-06 12:08:37 +0100

    adding slider and send colurs with additional parameter waiting time

--- lib/BluetoothListen.dart
+++ lib/BluetoothListen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_ble_cp/BleDeviceSelectionWidget.dart';
-
+import "dart:typed_data";
 
 class BleListenWidget extends StatefulWidget {
   @override
@@ -17,6 +17,7 @@ class _BleListenWidget extends State {
     Future.delayed(Duration.zero, () => _initializeBleDevices());
   }
 
+  double val = 6;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +30,6 @@ class _BleListenWidget extends State {
       body: Container(
         child: Center(
 
-
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -40,9 +40,24 @@ class _BleListenWidget extends State {
                 textColor: Colors.blue,
                 padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                 splashColor: Colors.grey,
-              )
+              ),
+
+
+              Slider(
+                value: val,
+                onChanged: (value) {
+                  setState(() {
+                    val = value;
+                  });
+                },
+                min: 1,
+                max: 10,
+                activeColor: Colors.green,
+              ),
+
             ],
           ),
+
         ),
       ),
     );
@@ -50,61 +65,63 @@ class _BleListenWidget extends State {
 
   // This code is called when the first button is pressed.
   void _firstButtonPressed() {
-    print("Button pressed.");
-    _sendColorToAllDevices(red: 250, green: 0, blue: 0);
+    print("Button 'Send Color to All Devices' pressed.");
+    var waiting_time_int = val.toInt();
+    print("Slider Value: $waiting_time_int");
+    _sendBleDataToAllDevices(red: 250, green: 0, blue: 0, wait: waiting_time_int);
+    //_sendBleDataToAllDevices(red: 250, green: 0, blue: 0, wait: 1);
   }
 
-  void _initializeBleDevices({int red: 250, int green: 30, int blue: 50}) {
+  void _initializeBleDevices() {
     print("Available devices: ${bleDevices.length}");
 
     for (int i = 0; i < bleDevices.length; i++) {
       var device = bleDevices[i];
-      device.tx.value
-          .listen((value) => _onEvent(GameEventType.bluetoothReceived, deviceNumber: i, bluetoothData: value));
+      device.tx.value.listen((listen_value_cp) => _onEvent(GameEventType.bluetoothReceived, deviceNumber: i, bluetoothData: listen_value_cp));
+
     }
   }
 
-  void _sendColorToAllDevices({int red: 250, int green: 30, int blue: 50}) {
+  void _sendBleDataToAllDevices({int red: 250, int green: 30, int blue: 50, int wait: 1}) {  // double wait: 1
 
     for (int i = 0; i < bleDevices.length; i++) {
-      _sendColorToDevice(i, red: red, green: green, blue: blue);
+      _sendBleDataToDevice(i, red: red, green: green, blue: blue, wait: wait); //
     }
 
     bleDevices.forEach((device) {
       // Do something for alle devices.
     });
   }
-
-  void _sendColorToDevice(int deviceNumber, {int red: 250, int green: 30, int blue: 50}) {
-    var color = [red, green, blue];
+  // Todo let waiting time be a double -> problems with device.rx.write
+  void _sendBleDataToDevice(int deviceNumber, {int red: 250, int green: 30, int blue: 50, int wait: 1}) {
+    var data = [red, green, blue, wait];
 
     if (deviceNumber <= bleDevices.length) {
       var device = bleDevices[deviceNumber];
       try {
-        device.rx.write(color, withoutResponse: true);
+        device.rx.write(data, withoutResponse: true);
       } catch (e) {
-        print("Could not send color to device: $e");
+        print("CP: Could not send color to device: $e");
       }
     } else {
       print("This device ($deviceNumber) doesn't exist.");
     }
   }
 
-  void _onEvent(GameEventType type, {int deviceNumber, List<int> bluetoothData, int timerData}) {
-    print("type: $type, deviceNumber: $deviceNumber, bluetoothData: $bluetoothData, timerData: $timerData");
+  void _onEvent(GameEventType type, {int deviceNumber, List<int> bluetoothData}) {  // List<int> bluetoothData
+    print("BLE Data:");
+    print(bluetoothData);
 
+    // Uint8List intBytes = Uint8List.fromList(bluetoothData.toList());
+    // List<double> floatList = intBytes.buffer.asFloat32List();
+    // print(floatList);
 
-
-/*    if (deviceNumber != null && bluetoothData != null && bluetoothData.isNotEmpty && bluetoothData.first == 99) {
-      // This Timer will execute the _onEvent after 1 second
-      _timer = Timer(Duration(seconds: 1), () => _onEvent(GameEventType.timer, timerData: 25));
-    } else if (type == bluetoothData) {
-      _timer.cancel();
-    }*/
-
-    if (bluetoothData.length == 1) { // GameEventType.bluetoothReceived // && deviceNumber == 0
-      print("JA in dieser Schleife!!");
-    }
+    // print("type: $type, deviceNumber: $deviceNumber, bluetoothData: $bluetoothData, timerData: $timerData");
 
   }
+
+
 }
+
+
+
