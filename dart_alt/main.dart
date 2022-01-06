ff815350040ab778f7ee9ad306c9c7d2e9447b24
cpechhacker <christian.pechhacker@gmail.com>
2022-01-06 12:05:02 +0100

    removed old files

--- dart_alt/main.dart
+++ dart_alt/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
-import 'HomeWidget.dart';
+import '../lib/HomeWidget.dart';
 
 void main() => runApp(MyApp());
 
